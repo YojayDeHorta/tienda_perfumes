0e9e1d6a73d72c2aec5fa7809b312f4cdfa0d39f
yojay esteban <yojay1000@gmail.com>
2021-06-07 15:21:17 -0500

    separacion del admin

---
+++ view/admin/js/1.js
@@ -0,0 +1,105 @@
+
+/*-------------------VENTANA MODAL---------------------------*/
+$(document).ready(function () {
+
+  function showPopup(){
+      $('.Ventana-Modal').addClass('show');
+      $('.Sub-Ventana_Modal').addClass('show');
+  }
+
+  $("#close").click(function(){
+      $('.Ventana-Modal').removeClass('show');
+      $('.Sub-Ventana_Modal').removeClass('show');
+  });
+
+  $(".btn-abrir").click(showPopup);
+
+  setTimeout(showPopup, 1000);
+
+});
+
+//-------------------VENTANA MODAL--------------------------
+
+//-----------------------------SLIDE---------------------------------------------
+const myslide = document.querySelectorAll(".myslide"),
+  dot = document.querySelectorAll(".Dirrecion");
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 3000);
+
+function autoSlide() {
+  counter += 1;
+  slidefun(counter);
+}
+
+function plusSlides(n) {
+  counter += n;
+  slidefun(counter);
+  resetTimer();
+}
+
+function currentSlide(n) {
+  counter = n;
+  slidefun(counter);
+  resetTimer();
+}
+
+function resetTimer() {
+  clearInterval(timer);
+  timer = setInterval(autoSlide, 1000);
+}
+
+function slidefun(n) {
+  let i;
+  for (i = 0; i < myslide.length; i++) {
+    myslide[i].style.display = "none";
+  }
+  for (i = 0; i < dot.length; i++) {
+    dot[i].className = dot[i].className.replace(" active", "");
+  }
+  if (n > myslide.length) {
+    counter = 1;
+  }
+  if (n < 1) {
+    counter = myslide.length;
+  }
+  myslide[counter - 1].style.display = "block";
+  dot[counter - 1].className += " active";
+}
+
+//-----------------------------SLIDE---------------------------------------------
+
+
+
+/*--------------TABS----------------*/
+$(".tab-list").on("click", ".tab", function(event) {
+	event.preventDefault();
+
+  $(".tab").removeClass("active");
+	$(".tab-content").removeClass("show");
+
+	$(this).addClass("active");
+	$($(this).attr('href')).addClass("show");	
+});
+
+/*--------------TABS----------------*/
+
+/*--------------TABS-Admin----------------*/
+function Tabs_Principal(evt, Tabs) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(Tabs).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+
+/*--------------TABS----------------*/
